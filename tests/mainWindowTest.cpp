ca6517113b533560973408bf77a4c78fe0e7cf40
Leonardo Ayala <l.menjivar@dkfz-heidelberg.de>
2024-04-11 12:49:58 +0200

    Added tests for writeLogHeader

--- tests/mainWindowTest.cpp
+++ tests/mainWindowTest.cpp
@@ -37,3 +37,26 @@ TEST_F(MockMainWindowTest, EnableUI) {
     ASSERT_NO_THROW(mockMainWindow->EnableUi(true));
     ASSERT_NO_THROW(mockMainWindow->EnableUi(false));
 }
+
+TEST_F(MockMainWindowTest, WriteLogHeaderTest)
+{
+    // Code to clear or delete the log file before starting the test
+    QFile::remove(LOG_FILE_NAME);
+
+    // Call the method under test
+    mockMainWindow->WriteLogHeader();
+
+    // Now, read the content of the log file
+    QFile file(LOG_FILE_NAME);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream in(&file);
+        EXPECT_EQ(" git hash: " + QString::fromLatin1(libfiveGitRevision()), in.readLine());
+        EXPECT_EQ(" git branch: " + QString::fromLatin1(libfiveGitBranch()), in.readLine());
+        EXPECT_EQ(" git tags matching hash: " + QString::fromLatin1(libfiveGitVersion()), in.readLine());
+        file.close();
+        QFile::remove(LOG_FILE_NAME);
+    } else {
+        // Handle error: Couldn't open the file, test should fail
+        ASSERT_TRUE(false);
+    }
+}
